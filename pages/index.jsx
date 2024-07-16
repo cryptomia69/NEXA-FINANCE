737aa94ae8594edfc4fae96475190e67aa86bee1
Tanvir Mahmud <90540435+cryptomia69@users.noreply.github.com>
2024-07-16 13:56:49 +0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,7 @@ export default function Home() {
 						</Link>
 					</nav>
 					<div className="flex">
-						<Link href="Link here">
+						<Link href="https://t.me/nexafibot/app">
 							<a className="px-5 py-3 rounded-xl btn-normal ml-3 font-semibold text-sm hidden md:block">
 								Play Game
 							</a>
@@ -63,7 +63,7 @@ export default function Home() {
 								Engage in staking and earn passive income through strategic investments and resource management.
 								Join NEXA FINANCE and forge your path to financial success.
 							</p>
-							<Link href="link here">
+							<Link href="https://t.me/nexafibot/app">
 								<a className="px-8 py-3 rounded-xl btn-normal font-semibold my-5">
 									Play Now
 								</a>
